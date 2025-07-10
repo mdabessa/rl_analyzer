a44e7ad943fa8eddf40b6f5a27d28a789f7a31bc
Matheus Bessa <mdabessa@gmail.com>
2025-07-10 14:25:01 -0300

    Add get_ping and get_replays functions; update last endpoint to utilize them

--- src/ballchasing_api.py
+++ src/ballchasing_api.py
@@ -1,7 +1,20 @@
 import requests
 
 
+def get_ping(authorization: str) -> requests.Response:
+    ping_url = "https://ballchasing.com/api"
+    ping = requests.get(ping_url, headers={"Authorization": authorization})
+    return ping
+
+
 def get_replay(replay_id: str, authorization: str) -> requests.Response:
     replay_url = "https://ballchasing.com/api/replays/" + str(replay_id)
     replay = requests.get(replay_url, headers={"Authorization": authorization})
     return replay
+
+
+def get_replays(authorization: str, filters: dict = None) -> requests.Response:
+    replays = requests.get("https://ballchasing.com/api/replays", headers={"Authorization": authorization}, 
+                    params=filters)
+    return replays
+
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, HTTPException, Header
 from fastapi.middleware.cors import CORSMiddleware
+import time
 
 from .analyzer import Model
-from .ballchasing_api import get_replay
+from .ballchasing_api import get_replay, get_ping, get_replays
 
 
 app = FastAPI()
@@ -41,6 +42,43 @@ async def replay(replay_id: str, authorization: str | None = Header(default=None
             status_code=400, detail=f"Replay playlist is not compatible."
         )
 
+    model.analyze(replay)
+
+    return replay
+
+
+@app.get("/last")
+async def last(authorization: str | None = Header(default=None)):
+    if authorization is None:
+        raise HTTPException(
+            status_code=401, detail={"error": "Missing authorization token"}
+        )
+
+    ping = get_ping(authorization)
+    ping.raise_for_status()
+
+    time.sleep(0.5)
+
+    replays = get_replays(authorization, filters={"player-name": ping.json()["name"], "count": 1})
+    replays.raise_for_status()
+
+    time.sleep(0.5)
+
+    replay_id = replays.json()['list'][0]['id']
+
+    time.sleep(0.5)
+
+    response = get_replay(replay_id, authorization)
+    response.raise_for_status()
+
+    replay = response.json()
+
+    model = Model.get_model(replay["playlist_id"])
+    if model is None:
+        raise HTTPException(
+            status_code=400, detail=f"Replay playlist is not compatible."
+        )
+    
     model.analyze(replay)
 
     return replay
